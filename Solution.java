bb75dd4397d176a4ecb1313614dd20711765de64
Sushant Sutar <106394705+SushantSutar@users.noreply.github.com>
2025-08-06 14:46:19 +0530

    equilateral triangle * print

--- Solution.java
+++ Solution.java
@@ -1311,3 +1311,24 @@ public class DecimalToAll {
         scanner.close();
     }
 }
+
+
+// equilateral triangle * print
+import java.util.Scanner;
+publuc class equilateral{
+	public static void main (String[] args) {
+	        Scanner sc=new Scanner(System.in);	
+	    	System.out.println("enter number : ");
+	    	int n=sc.nextInt();
+	    	for(int i=1;i<=n-1;i++){
+	    		for(int j=1;j<=n-i;j++){
+	    			System.out.print(" ");
+	    		}
+	    		for(int j=1;j<=i*2-1;j++){
+	    			System.out.print("*");
+	    		}
+	    		System.out.println();
+	    	}
+    	}
+
+}
