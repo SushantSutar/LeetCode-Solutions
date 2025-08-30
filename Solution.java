4f02f28f70c20060ef364c9e9c2dcfb3bd1fc4eb
Sushant Sutar <106394705+SushantSutar@users.noreply.github.com>
2025-08-30 12:05:03 +0530

    Solved Leetcode 3120

--- Solution.java
+++ Solution.java
@@ -2927,3 +2927,48 @@ class Solution {
 }
 
 
+
+//Leetcode 3120. Count the Number of Special Characters I
+You are given a string word. A letter is called special if it appears both in lowercase and uppercase in word.
+Return the number of special letters in word.
+
+Example 1:
+Input: word = "aaAbcBC"
+Output: 3
+Explanation:
+The special characters in word are 'a', 'b', and 'c'.
+Example 2:
+Input: word = "abc"
+Output: 0
+Explanation:
+No character in word appears in uppercase.
+Example 3:
+Input: word = "abBCab"
+Output: 1
+Explanation:
+The only special character in word is 'b'.
+
+	
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        Set<Character> smallSet = new HashSet<>();
+        Set<Character> capitalSet = new HashSet<>();
+        
+        for (char c : word.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                smallSet.add(c);
+            } else {
+                capitalSet.add(c);
+            }
+        }
+        
+        int count = 0;
+        for (char c : smallSet) {
+            if (capitalSet.contains(Character.toUpperCase(c))) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
